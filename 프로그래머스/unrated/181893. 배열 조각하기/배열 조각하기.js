4b8cd16423bc9b7431136c130ad7adf094e11cff
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2023-12-04 13:16:07 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -1,11 +1,14 @@
+// 짝수 인덱스 -> 뒷부분 버리기
+// 홀수 인덱스 -> 앞부분 버리기
 function solution(arr, query) {
   for (let i = 0; i < query.length; i++) {
     let index = query[i];
 
     if (i % 2 === 0) {
-      // 짝수 인덱스의 쿼리 -> 뒷부분 자르기
-      arr.splice(index + 1, arr.length - (index + 1));
+      // 짝수 인덱스의 쿼리 -> 뒷부분 삭제하기
+      arr.splice(index + 1);
     } else {
+      // 홀수 인덱스의 쿼리 -> 앞부분 삭제하기
       arr.splice(0, index);
     }
   }
